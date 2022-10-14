cfa8e2930152e754d712a9a2293e15f67966a452
Akbar <akber.sharifov2004@gmail.com>
2022-10-14 12:53:49 +0300

    add film search

---
+++ film search/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using WpfApp3.Model;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void search_button_Click(object sender, RoutedEventArgs e)
+        {
+            string ResultSearch = await FilmSearchDownoloadService.FindFilm(search_box.Text);
+            var titlesearch = DeserializeFilmSearchService.deserializeFilmInfo(ResultSearch);
+            if (titlesearch != null)
+            {
+                title_name_text.Text = titlesearch.Country;
+                ratings_title_box.Text = titlesearch.Ratings?[0].Value ?? "not found";
+                plot_text.Text = titlesearch.Plot;
+                actors_text.Text = titlesearch.Actors;
+                genre_text.Text = titlesearch.Genre;
+                if (titlesearch.Poster != null)
+                {
+                    using WebClient client = new WebClient();
+                    var downoloadPicture = client.DownloadData(new Uri(titlesearch.Poster)); // возможная причина ошибки
+                    //MemoryStream memoryStream = new MemoryStream(downoloadPicture);
+                    //title_image.Source = ;
+                }
+            }
+            search_box.Text = "";
+        }
+    }
+}
---
+++ film search/WpfApp3/Model/FilmSearchDownoloadService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3.Model
+{
+    public static class FilmSearchDownoloadService
+    {
+        private static string key = "6c31049f";
+        private static string url = @"https://www.omdbapi.com/?t=";
+        public static async Task<string> FindFilm(string name)
+        {
+            WebClient client = new WebClient();
+            return await client.DownloadStringTaskAsync($@"{url}{name}&apikey={key}");
+        }
+    }
+}
